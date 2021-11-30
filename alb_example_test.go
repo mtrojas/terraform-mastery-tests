3e2dc5a4b885d0210226957878b2834e32b23774
MT <mtrojas@protonmail.com>
2021-11-30 18:47:28 +0000

    initial commit

---
+++ alb_example_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"crypto/tls"
+	"fmt"
+	"testing"
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAlbExample(t *testing.T) {
+	opts := &terraform.Options{
+		// Path pointing to the ALB Example directory
+		TerraformDir: "/Users/mtrojas/Documents/terraform-mastery/modules/examples/alb",
+	}
+
+	// Clean up everything at the end of the test
+	defer terraform.Destroy(t, opts)
+
+	// Deploy the example
+	terraform.InitAndApply(t, opts)
+
+	// Get the URL of the ALB
+	albDnsName := terraform.OutputRequired(t, opts, "alb_dns_name")
+	url := fmt.Sprintf("http://%s", albDnsName)
+
+	// Test that the ALB's default action is working and returns a 404
+	expectedStatus := 404
+	expectedBody := "404: page not found"
+
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+	tlsConfig := tls.Config{}
+
+	http_helper.HttpGetWithRetry(
+		t,
+		url,
+		&tlsConfig,
+		expectedStatus,
+		expectedBody,
+		maxRetries,
+		timeBetweenRetries,
+	)
+}
