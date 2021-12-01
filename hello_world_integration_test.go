460a35cbfc0f89e7337fe589d31495795aae207d
MT <mtrojas@protonmail.com>
2021-12-01 20:59:49 +0000

    feat: add testing by stages and retries

--- hello_world_integration_test.go
+++ hello_world_integration_test.go
@@ -10,6 +10,7 @@ import (
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 // TODO: move to terraform-mastery and change to relative paths
@@ -69,6 +70,14 @@ func createHelloOpts(dbOpts *terraform.Options, terraformDir string) *terraform.
 			"db_remote_state_key":    dbOpts.BackendConfig["key"],
 			"environment":            dbOpts.Vars["db_name"],
 		},
+
+		// Retry up to three times, with 5 seconds between retries,
+		// on known errors
+		MaxRetries:         3,
+		TimeBetweenRetries: 5 * time.Second,
+		RetryableTerraformErrors: map[string]string{
+			"RequestError: send request failed": "Throttling issue?",
+		},
 	}
 }
 
@@ -90,4 +99,57 @@ func validateHelloApp(t *testing.T, helloOpts *terraform.Options) {
 			return status == 200 && strings.Contains(body, "My new text")
 		},
 	)
+}
+
+func TestHelloWorldAppStageWithStages(t *testing.T) {
+	t.Parallel()
+
+	stage := test_structure.RunTestStage
+
+	// Deploy the MySQL DB
+	defer stage(t, "teardown_db", func() { teardownDb(t, dbDirStage) })
+	stage(t, "deploy_db", func() { deployDb(t, dbDirStage) })
+
+	// Deploy the hello-world-app
+	defer stage(t, "teardown_app", func() { teardownApp(t, appDirStage) })
+	stage(t, "deploy_app", func() { deployApp(t, dbDirStage, appDirStage) })
+
+	// Validate the hello-world-app works
+	stage(t, "validate_app", func() { validateApp(t, appDirStage) })
+}
+
+func deployDb(t *testing.T, dbDir string) {
+	dbOpts := createDbOpts(t, dbDir)
+
+	// Save data to disk so that other test stages executed
+	// at a later time can read the data back in
+	test_structure.SaveTerraformOptions(t, dbDir, dbOpts)
+
+	terraform.InitAndApply(t, dbOpts)
+}
+
+func teardownDb(t *testing.T, dbDir string) {
+	dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+	defer terraform.Destroy(t, dbOpts)
+}
+
+func deployApp(t *testing.T, dbDir string, helloAppDir string) {
+	dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+	helloOpts := createHelloOpts(dbOpts, helloAppDir)
+
+	// Save data to disk so that other test stages executed
+	// at a later time can read the data back in
+	test_structure.SaveTerraformOptions(t, helloAppDir, helloOpts)
+
+	terraform.InitAndApply(t, helloOpts)
+}
+
+func teardownApp(t *testing.T, helloAppDir string) {
+	helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+	defer terraform.Destroy(t, helloOpts)
+}
+
+func validateApp(t *testing.T, hellopAppDir string) {
+	helloOpts := test_structure.LoadTerraformOptions(t, hellopAppDir)
+	validateHelloApp(t, helloOpts)
 }
